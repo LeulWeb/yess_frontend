42e8b0065c5f9561ed9ee4a4cf299407c88dab35
Haileopia <haileamlakwaleligne3910@gmail.com>
2024-01-09 14:38:39 -0800

    update the helder job section

--- app/helder/page.js
+++ app/helder/page.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import gopa from '../assets/fewis.jpg';
+import gopa from '../assets/helder.png';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Fewis = () => {
+
+const Helder = () => {
   return (
     <div>
         <div className='flex flex-start items-center justify-start gap-2'>
             <Image src={gopa} alt="FMF" width={196} height={201} />
             <div className="ml-4">
-                <p className='text-2xl font-semibold leading-13 text-left'>Fewis Digital Medical Solutions</p>
-                <p className='text-2xl font-normal leading-13 text-left text-[#260000]'>Medical Solutions</p>
+                <p className='text-2xl font-semibold leading-13 text-left'>Helder Technologies Solution</p>
+                <p className='text-2xl font-normal leading-13 text-left text-[#260000]'>IT company</p>
             </div>
         </div>
         <div>
         <p className='pop text-2xl font-light text-center px-4 sm:px-8 md:px-12 lg:mx-28 mb-10'>
-        Fewis Digital Medical Solutions offers an internship for those interested in a medical profession. Apply below for a chance to gain valuable experience in our dynamic and innovative medical environment. Work with cutting-edge technologies, learn from seasoned professionals, and contribute to digital solutions in the medical sector. This internship is tailored for students or recent graduates seeking practical exposure and a meaningful educational experience. To apply, use the form below, including your resume, a cover letter expressing your interest, and relevant references. Take the first step towards a rewarding internship with Fewis Digital Medical Solutions.
+        Helder Technologies Solution PLC is actively seeking a talented Mobile App Developer to join our team. If you are passionate about contributing to a dynamic startup environment and have the skills to excel in mobile app development, we invite you to consider a rewarding career with Helder Technologies Solution. Your expertise will play a crucial role in shaping innovative solutions and driving the success of our projects. Join us in creating cutting-edge mobile applications that make a significant impact. Welcome to Helder Technologies Solution – where your skills meet opportunity.
 
  </p>
         </div>
@@ -27,7 +28,7 @@ const Fewis = () => {
         </div>
         <div className='flex flex-col md:flex-row gap-10  justify-center items-center py-2 '>
             <p className="text-2xl font-normal leading-13 text-left">Deadline:</p>
-            <p className="text-2xl font-normal leading-13 text-left">Dec 30, 2024</p>
+            <p className="text-2xl font-normal leading-13 text-left">Aug 1, 2024</p>
         </div>
         <div className='flex flex-col md:flex-row gap-10  justify-center items-center py-2 '>
             <p className="text-2xl font-normal leading-13 text-left">Salary/Compensation</p>
@@ -35,7 +36,7 @@ const Fewis = () => {
         </div>
         <div className='flex flex-col md:flex-row gap-10  justify-center items-center py-2 '>
             <p className="text-2xl font-normal leading-13 text-left">Vacancies</p>
-            <p className="text-2xl font-bold leading-13 text-left">2</p>
+            <p className="text-2xl font-bold leading-13 text-left">1</p>
         </div>
         <div className='flex flex-col md:flex-row gap-10  justify-center items-center py-2 '>
             <p className="text-2xl font-normal leading-13 text-left">Applicant Needed:</p>
@@ -70,4 +71,4 @@ const Fewis = () => {
   )
 }
 
-export default Fewis;
+export default Helder;
