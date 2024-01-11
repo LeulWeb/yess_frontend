4878f500b35c7f259e94e31335e786357ca60bd4
Haileopia <haileamlakwaleligne3910@gmail.com>
2024-01-10 21:58:48 -0800

    updates made by ashe

--- app/components/home/Documentary.jsx
+++ app/components/home/Documentary.jsx
@@ -5,23 +5,13 @@ import yes from '../../assets/you.jpg';
 const Documentary = () => {
   return (
     <div className="doc bg-cover bg-no-repeat h-full text-white justify-center items-center flex flex-col lg:px-10 pb-10">
-      <h1 className="tes font-bold text-center lg:-ml-10">Documentary</h1>
       <div className="flex flex-col md:flex-row">
         <div className="md:w-1/2 lg:w-2/5">
           <h1 className="text-[40px] font-bold text-center lg:-ml-10">
             Empowering Dreams
           </h1>
           <p className="pop px-2 font-light emb mt-5">
-            Embark on a visual journey that unfolds the narratives of young
-            individuals whose lives have been touched by Yess Ethiopia. From
-            overcoming challenges to achieving dreams, this documentary captures
-            the essence of our commitment to youth empowerment.
-            <br />
-            Join us as we celebrate success stories, highlight the strength of
-            community involvement, and showcase the innovative approaches that
-            define Yess Ethiopia's mission. This single documentary encapsulates
-            the heart and soul of our organization, inspiring hope and positive
-            change.
+            This single documentary encapsulates the heart and soul of our organization, inspiring hope and positive change
           </p>
         </div>
         <div className="md:col-span-1 lg:col-span-2 md:w-1/2 lg:w-3/5">
--- app/components/home/Input.jsx
+++ app/components/home/Input.jsx
@@ -32,46 +32,52 @@ const Input = () => {
   const images = [volu, car, scho, train, boost, talent];
 
   return (
-  <div className="off bg-cover bg-no-repeat h-full">
-    <h1 className='tes text-center py-10 font-semibold'>Our Offerings</h1>
+    <div className="off bg-cover bg-no-repeat h-full">
+      <h1 className="tes text-center py-10 font-semibold">Our Offerings</h1>
       <div className=" w-full flex flex-col md:flex-row overflow-x-hidden justify-center items-center">
-      <div className="lg:w-2/5 md:w-1/2 w-full px-1">
-        <p className='font-bold text-center items-center text-[40px] py-5'>Yess Ethiopia</p>
-        <p className="pop px-6 text-center text-[25px]">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
-          asperiores minus quia beatae itaque. Culpa inventore voluptas enim
-          fuga soluta quisquam sed nostrum nihil aspernatur dicta ullam
-          similique, beatae assumenda.
-        </p>
-      </div>
-      <div className="lg:w-[65%] md:w-[50%] w-[100%]">
-        <div
-          className="carousel carousel-vertical rounded-box h-[600px] w-full overflow-y-auto  mx-3"
-          ref={carouselRef}
-        >
-          {images.map((image, index) => (
-            <div
-              key={index}
-              className={`carousel-item h-[200px] w-[607px] ${
-                centeredInput === index + 1
-                  ? 'centered mx-10 h-[217px] w-[802]'
-                  : 'mx-20'
-              }`}
-              onClick={() => handleInputChange(index + 1)}
-            >
-              <Image
-                src={image}
-                alt={`Carousel Item ${index + 1}`}
-                height={300}
-                width={400}
-                className=" w-full my-3 mx-10"
-              />
-            </div>
-          ))}
+        <div className="lg:w-2/5 md:w-1/2 w-full px-1">
+          <p className="font-bold text-center items-center text-[40px] py-5">
+            Yess Ethiopia
+          </p>
+          <p className="pop px-6 text-center text-[25px]">
+            YessEthiopia offers a range of services including voluntarism,
+            career support, scholarships, comprehensive training, startup
+            boosting, and talent linking. We foster community involvement,
+            provide career guidance, offer scholarship opportunities, and
+            empower individuals with necessary skills. Our Startup Boost section
+            assists startups with expert mentorship, and our Yess Talent service
+            connects skilled graduates with market needs. Together, we're
+            transforming lives and communities!
+          </p>
+        </div>
+        <div className="lg:w-[65%] md:w-[50%] w-[100%]">
+          <div
+            className="carousel carousel-vertical rounded-box h-[600px] w-full overflow-y-auto  mx-3"
+            ref={carouselRef}
+          >
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className={`carousel-item h-[200px] w-[607px] ${
+                  centeredInput === index + 1
+                    ? 'centered mx-10 h-[217px] w-[802]'
+                    : 'mx-20'
+                }`}
+                onClick={() => handleInputChange(index + 1)}
+              >
+                <Image
+                  src={image}
+                  alt={`Carousel Item ${index + 1}`}
+                  height={300}
+                  width={400}
+                  className=" w-full my-3 mx-10"
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
