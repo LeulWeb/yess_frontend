dc1fb77ea81509751c5963931f88d6061231fec7
Haileopia <haileamlakwaleligne3910@gmail.com>
2024-01-12 00:20:46 -0800

    update the home page intro section

--- app/components/home/HomeIntro.jsx
+++ app/components/home/HomeIntro.jsx
@@ -3,22 +3,22 @@ import bgImage from '../../assets/homi.jpg';
 
 const HomeIntro = () => {
   return (
-    <div className='overflow-x-hidden top-0'>
+    <div className="overflow-x-hidden top-0">
       <div
-        className=" relative  hero min-h-full lg:h-screen lg:overflow-y-hidden"
+        className=" relative  hero bg-cover bg-no-repeat min-h-full lg:h-screen lg:overflow-hidden"
         style={{ backgroundImage: `url(${bgImage})` }}
       >
-        <div className="flex flex-col items-center justify-center mt-20 lg:mt-10 text-[#000] py-10 lg:py-5  lg:mb-10">
-
-          <h1 className="text-[30px] lg:text-[40px] text-center font-bold py-5">
-            We empower the youth community <br /> through various means
-          </h1>
-          <p className="text-[20px] lg:text-[25px] text-center pb-5">
-            Our performance empowers the youth ,fostering <br /> success and
-            strength. Join us to contribute to <br />
-            community empowerment
-          </p>
-
+        <div className="absolute bg-black/70  min-h-full lg:h-screen bg-cover bg-no-repeat min-h-full lg:h-screen lg:overflow-y-hidden w-screen overflow-x-hidden">
+          <div className="flex flex-col items-center justify-center mt-28 lg:mt-36 text-white py-10 lg:py-5  lg:mb-10">
+            <h1 className="text-[30px] lg:text-[40px] text-center font-bold py-5">
+              We empower the youth community <br /> through various means
+            </h1>
+            <p className="text-[20px] lg:text-[25px] text-center pb-5">
+              Our performance empowers the youth ,fostering <br /> success and
+              strength. Join us to contribute to <br />
+              community empowerment
+            </p>
+          </div>
         </div>
       </div>
     </div>
