e9e8a968ad6510c1a6f5083877d4a2d2670da93d
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-25 13:43:09 +0300

    update the video directory

--- app/components/video/schos/Scho2.jsx
+++ app/components/video/schos/Scho2.jsx
@@ -10,7 +10,7 @@ const Scho2 = () => {
   return (
     <div>
       <h1 className="font-bold tes px-2 sm:px-4 md:px-12 lg:px-28 xl:px-32 py-10">
-       Scholarship
+        Scholarship
       </h1>
       <p className="text-[30px] font-bold text-start pl-4">Part</p>
       <div className="pb-10">
@@ -31,7 +31,9 @@ const Scho2 = () => {
                     Part 1
                   </p>
                   <p className="pl-5 text-start hidden md:flex">
-                  Scholarships open doors to education. They alleviate financial barriers, empowering individuals to pursue academic goals......
+                    Scholarships open doors to education. They alleviate
+                    financial barriers, empowering individuals to pursue
+                    academic goals......
                   </p>
                 </div>
                 <div className="flex flex-end justify-end items-center gap-10 ml-4">
@@ -40,7 +42,7 @@ const Scho2 = () => {
                       <BsPlayCircle
                         size={50}
                         className="text-gray-400 font-light hover:text-[#0041ff]"
-                      />{' '}
+                      />{" "}
                     </button>
                   </Link>
                 </div>
@@ -48,9 +50,8 @@ const Scho2 = () => {
             </div>
           </div>
 
-
-           {/* Item 2 */}
-           <div className="mx-10 py-5 h-[500px] lg:h-[294px] overflow-hidden">
+          {/* Item 2 */}
+          <div className="mx-10 py-5 h-[500px] lg:h-[294px] overflow-hidden">
             <div className="flex flex-col md:flex-row gap-10  border-2 border-gray-300  rounded-xl">
               <Image
                 src={eq}
@@ -65,7 +66,9 @@ const Scho2 = () => {
                     Part 2
                   </p>
                   <p className="pl-5 text-start hidden md:flex">
-                  Scholarships open doors to education. They alleviate financial barriers, empowering individuals to pursue academic goals......
+                    Scholarships open doors to education. They alleviate
+                    financial barriers, empowering individuals to pursue
+                    academic goals......
                   </p>
                 </div>
                 <div className="flex flex-end justify-end items-center gap-10 ml-4">
@@ -74,7 +77,7 @@ const Scho2 = () => {
                       <BsPlayCircle
                         size={50}
                         className="text-gray-400 font-light hover:text-[#0041ff]"
-                      />{' '}
+                      />{" "}
                     </button>
                   </Link>
                 </div>
@@ -82,9 +85,8 @@ const Scho2 = () => {
             </div>
           </div>
 
-
- {/* Item 3 */}
- <div className="mx-10 py-5 h-[500px] lg:h-[294px] overflow-hidden">
+          {/* Item 3 */}
+          <div className="mx-10 py-5 h-[500px] lg:h-[294px] overflow-hidden">
             <div className="flex flex-col md:flex-row gap-10  border-2 border-gray-300  rounded-xl">
               <Image
                 src={eq}
@@ -99,7 +101,9 @@ const Scho2 = () => {
                     Part 3
                   </p>
                   <p className="pl-5 text-start hidden md:flex">
-                  Scholarships open doors to education. They alleviate financial barriers, empowering individuals to pursue academic goals......
+                    Scholarships open doors to education. They alleviate
+                    financial barriers, empowering individuals to pursue
+                    academic goals......
                   </p>
                 </div>
                 <div className="flex flex-end justify-end items-center gap-10 ml-4">
@@ -108,17 +112,16 @@ const Scho2 = () => {
                       <BsPlayCircle
                         size={50}
                         className="text-gray-400 font-light hover:text-[#0041ff]"
-                      />{' '}
+                      />{" "}
                     </button>
                   </Link>
                 </div>
               </div>
             </div>
           </div>
-        
 
-         {/* Item 4 */}
-         <div className="mx-10 py-5 h-[500px] lg:h-[294px] overflow-hidden">
+          {/* Item 4 */}
+          <div className="mx-10 py-5 h-[500px] lg:h-[294px] overflow-hidden">
             <div className="flex flex-col md:flex-row gap-10  border-2 border-gray-300  rounded-xl">
               <Image
                 src={eq}
@@ -133,16 +136,18 @@ const Scho2 = () => {
                     Part 4
                   </p>
                   <p className="pl-5 text-start hidden md:flex">
-                  Scholarships open doors to education. They alleviate financial barriers, empowering individuals to pursue academic goals......
+                    Scholarships open doors to education. They alleviate
+                    financial barriers, empowering individuals to pursue
+                    academic goals......
                   </p>
                 </div>
                 <div className="flex flex-end justify-end items-center gap-10 ml-4">
-                  <Link href="/vidsho4">
+                  <Link href="/vidscho4">
                     <button>
                       <BsPlayCircle
                         size={50}
                         className="text-gray-400 font-light hover:text-[#0041ff]"
-                      />{' '}
+                      />{" "}
                     </button>
                   </Link>
                 </div>
@@ -150,9 +155,8 @@ const Scho2 = () => {
             </div>
           </div>
 
-
-{/* Item 5 */}
-<div className="mx-10 py-5 h-[500px] lg:h-[294px] overflow-hidden">
+          {/* Item 5 */}
+          <div className="mx-10 py-5 h-[500px] lg:h-[294px] overflow-hidden">
             <div className="flex flex-col md:flex-row gap-10  border-2 border-gray-300  rounded-xl">
               <Image
                 src={eq}
@@ -167,7 +171,9 @@ const Scho2 = () => {
                     Part 5
                   </p>
                   <p className="pl-5 text-start hidden md:flex">
-                  Scholarships open doors to education. They alleviate financial barriers, empowering individuals to pursue academic goals......
+                    Scholarships open doors to education. They alleviate
+                    financial barriers, empowering individuals to pursue
+                    academic goals......
                   </p>
                 </div>
                 <div className="flex flex-end justify-end items-center gap-10 ml-4">
@@ -176,14 +182,13 @@ const Scho2 = () => {
                       <BsPlayCircle
                         size={50}
                         className="text-gray-400 font-light hover:text-[#0041ff]"
-                      />{' '}
+                      />{" "}
                     </button>
                   </Link>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
