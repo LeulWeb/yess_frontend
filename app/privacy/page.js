34ead8fb5555fde85fa90df2bfeccd41facd8224
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-29 17:35:07 +0300

    add privacy page

---
+++ app/privacy/page.js
@@ -0,0 +1,181 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="mx-2 sm:mx-6 md:mx-12 lg:mx-20 px-2 sm:px-6 md:px-12 lg:px-20 flex flex-col justify-center items-center  py-10">
+      <div className="px-2 sm:px-4 md:px-12 lg:px-16 pb-16">
+        <p className="text-[30px] md:text-[35px] lg:text-[40px] font-bold text-center px-2 sm:px-6 md:px-12 lg:px-28">
+          Youth and Education Support Service Organization Policy Page
+        </p>
+        <p className="text-[20px] md:text-[24px] lg:text-[28px] text-center font-bold py-3">
+          Mission Statement
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          At Youth and Education Support Service, we are dedicated to empowering
+          youth through education, mentorship, and community engagement. Our
+          mission is to provide resources, support, and opportunities that
+          enable young individuals to reach their full potential and become
+          active contributors to society.
+        </p>
+        <br />
+        <br />
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          This page outlines the principles, guidelines, and expectations
+          governing our organization's operations, activities, and interactions.
+          By adhering to these policies, we aim to maintain a safe, inclusive,
+          and supportive environment for all members, participants, volunteers,
+          and stakeholders involved in our programs and initiatives.
+        </p>
+        <p className="text-[21px] md:text-[24px] lg:text-[27px] text-start py-4 font-bold">
+          1. Code of Conduct:
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Respect: Treat all individuals with dignity, kindness, and respect,
+          regardless of differences in background, identity, or beliefs.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - Integrity: Act honestly, ethically, and responsibly in all
+          interactions and activities associated with organization
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Inclusivity: Embrace diversity and foster an inclusive environment
+          where everyone feels valued, included, and supported
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - Safety: Prioritize the safety and well-being of all participants,
+          ensuring that our programs and activities are conducted in secure
+          environments.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          Professionalism: Uphold professional standards in communication,
+          behavior, and decision-making, maintaining a positive image of YESS.
+        </p>
+
+        {/* Two */}
+        <p className="text-[21px] md:text-[24px] lg:text-[27px] text-start py-4 font-bold">
+          2. Child Protection Policy
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - YESS is committed to safeguarding the welfare of children and young
+          people involved in our programs
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - We adhere to strict protocols for the recruitment, screening, and
+          training of staff and volunteers who work directly with minors.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Any concerns or suspicions regarding child protection issues must be
+          reported immediately to designated authorities within the
+          organization.
+        </p>
+
+        {/* Three */}
+        <p className="text-[21px] md:text-[24px] lg:text-[27px] text-start py-4 font-bold">
+          3. Confidentiality and Privacy:
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Protect the confidentiality and privacy of all individuals
+          associated withYESS, including participants, volunteers, and donors.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - Personal information collected from participants and stakeholders
+          will be used solely for the purposes outlined in our privacy policy
+          and will not be shared with third parties without consent.
+        </p>
+
+        {/* Four */}
+        <p className="text-[21px] md:text-[24px] lg:text-[27px] text-start py-4 font-bold">
+          4. Anti-Discrimination and Harassment:
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - YESS is committed to providing an environment free from
+          discrimination, harassment, and bullying.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - Discrimination or harassment based on race, ethnicity, gender,
+          sexual orientation, religion, disability, or any other characteristic
+          is strictly prohibited.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Complaints of discrimination or harassment will be promptly
+          investigated, and appropriate action will be taken in accordance with
+          organizational policies and applicable laws.
+        </p>
+
+        {/* Five */}
+        <p className="text-[21px] md:text-[24px] lg:text-[27px] text-start py-4 font-bold">
+          5. Financial Integrity:
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - YESS is committed to maintaining the highest standards of financial
+          integrity and transparency.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - All financial transactions and records will be accurately
+          documented, reported, and audited in compliance with relevant laws and
+          regulations.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Donations and funds received will be utilized efficiently and
+          effectively to further the organization's mission and goals.
+        </p>
+
+        {/* Six */}
+        <p className="text-[21px] md:text-[24px] lg:text-[27px] text-start py-4 font-bold">
+          6. Social Media and Online Behavior:
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - When representing YESS online, members and volunteers are expected
+          to uphold the organization's values and principles.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - Respect the privacy and rights of others when posting content or
+          engaging in online discussions, and refrain from sharing sensitive or
+          confidential information.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Any online behavior that reflects negatively on YESS or contradicts
+          our mission and values may result in disciplinary action.
+        </p>
+
+        {/* Seven */}
+        <p className="text-[21px] md:text-[24px] lg:text-[27px] text-start py-4 font-bold">
+          7. Conflict of Interest:
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Avoid situations where personal interests may conflict with the
+          interests of YESS.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - Disclose any potential conflicts of interest promptly to designated
+          authorities within the organization.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - Act in the best interests of the organization and avoid actions that
+          may compromise its integrity or reputation.
+        </p>
+
+        {/* Eight */}
+        <p className="text-[21px] md:text-[24px] lg:text-[27px] text-start py-4 font-bold">
+          8. Compliance and Accountability
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - All members, volunteers, and stakeholders are expected to
+          familiarize themselves with and adhere to the policies and guidelines
+          outlined by YESS.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] py-3">
+          - Violations of organizational policies may result in disciplinary
+          action, up to and including termination of membership or
+          participation.
+        </p>
+        <p className="text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px]">
+          - YESS is committed to transparency and accountability in its
+          operations, and welcomes feedback and suggestions for improvement.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
