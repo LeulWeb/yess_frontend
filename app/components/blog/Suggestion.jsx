14c2a3913f9f5daad02928d15d6380b938b8fa2e
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-03-29 17:34:54 +0300

    update the suggestion page

--- app/components/blog/Suggestion.jsx
+++ app/components/blog/Suggestion.jsx
@@ -36,49 +36,53 @@ const Suggestion = () => {
     <div>
       <div className="py-10">
         <div className="flex justify-between items-center">
-          <p className="text-[40px] font-bold text-start px-2 sm:px-6 md:px-12 lg:px-28">
+          <p className="text-[30px] md:text-[35px] lg:text-[40px] font-bold text-start px-2 sm:px-6 md:px-12 lg:px-28">
             Suggestion Spot
           </p>
-          {/* <div className="flex justify-end items-center gap-2 hover:border-b-2 hover:border-[#0041ff] cursor-pointer duration-500 text-[#0041ff] px-2 sm:px-6 md:px-12 lg:px-28">
-                <p className="text-[30px]  text-start px-2 sm:px-6 md:px-12 lg:px-28 ">View More</p>
-                <FaArrowRight />
-                </div> */}
         </div>
         <div className="carousel carousel-center max-w-full p-4 space-x-10 rounded-box h-full ">
-           {news.map((items, index)=>{
-            return(
-                <div key={index} className="carousel-item shadow-xl border-gray-50 border-2 bg-[#0041ff] w-[350px] lg:w-[420px] rounded-box flex flex-col items-center justify-center h-full pb-10">
+          {news.map((items, index) => {
+            return (
+              <div
+                key={index}
+                className="carousel-item shadow-xl border-gray-50 border-2 bg-[#0041ff] w-[350px] lg:w-[420px] rounded-box flex flex-col items-center justify-center h-full pb-10"
+              >
                 <Image
                   src={items.image}
                   alt={items.title}
-                  width={500} height={300}
+                  width={500}
+                  height={300}
                   className="w-full h-[300px] rounded-xl"
                 />
                 <div className="py-3 px-3 flex flex-col items-center justify-start text-start">
-                <div className="  flex flex-col items-center justify-start flex-start">
-                <h1 className="text-white text-[20px] lg:text-[30px] font-bold text-start px-2 text-center line-clamp-1">{items.title}</h1>
-                </div>
-                <p className="text-white text-[12px] text-start pop font-light line-clamp-2 lg:line-clamp-3">
-                  {items.description} 
-                </p>
-                <div className="flex justify-between items-center gap-10 border-t-2 py-4 mt-4 border-white w-full text-white">
-                 <div>
-                 <div className="text-gray-200  text-[14px] flex gap-6 justify-start items-center flex-start">
-                <div className='flex justify-start items-center gap-2'>
-                <CiCalendarDate size={20}/>
-                 <p className="text-sm">{items.posted}</p>
-                </div>
-                 </div>
-                 </div>
-                </div>
+                  <div className="  flex flex-col items-center justify-start flex-start">
+                    <h1 className="text-white text-[20px] lg:text-[30px] font-bold text-start px-2 text-center line-clamp-1">
+                      {items.title}
+                    </h1>
+                  </div>
+                  <p className="text-white text-[12px] text-start pop font-light line-clamp-2 lg:line-clamp-3">
+                    {items.description}
+                  </p>
+                  <div className="flex justify-between items-center gap-10 border-t-2 py-4 mt-4 border-white w-full text-white">
+                    <div>
+                      <div className="text-gray-200  text-[14px] flex gap-6 justify-start items-center flex-start">
+                        <div className="flex justify-start items-center gap-2">
+                          <CiCalendarDate size={20} />
+                          <p className="text-sm">{items.posted}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <Link href={items.url} className="linko">
-                  <button className="w-full py-1 text-[20px] bg-white -mb-10 hover:bg-blue-200 text-[#0041ff] font-semibold rounded-xl">View more</button>
+                  <button className="w-full py-1 text-[20px] bg-white -mb-10 hover:bg-blue-200 text-[#0041ff] font-semibold rounded-xl">
+                    View more
+                  </button>
                 </Link>
               </div>
-            )
-           })}
-            </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
