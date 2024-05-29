dd552c6669b3fa4baf9e7d7d690cbf66bda3422e
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-29 17:14:31 +0300

    update the whole section with nice view.

--- app/components/donate/Don.jsx
+++ app/components/donate/Don.jsx
@@ -5,7 +5,6 @@ import you from '../../assets/homi.jpg';
 import pay from '../../assets/pay.jpg';
 import Link from 'next/link';
 
-
 const Don = () => {
   return (
     <div className="overflow-x-hidden overflow-y-visible hero">
@@ -14,20 +13,20 @@ const Don = () => {
           id="default-modal"
           tabIndex="-1"
           aria-hidden="false"
-          className=" overflow-y-auto overflow-x-hidden  bg-transparent top-0 flex flex-col justify-center items-center w-full"
+          className="overflow-y-auto overflow-x-hidden bg-transparent top-0 flex flex-col justify-center items-center w-full"
         >
           <div className="relative p-4 w-full max-w-2xl max-h-full">
-            {/* <!-- Modal content --> */}
+            {/* Modal content */}
             <div className="relative">
-              {/* <!-- Modal body --> */}
-              <div className="p-4 md:p-5 space-y-4 grid grid-cols-1 md:grid-cols-2 gap-10 h-full ">
+              {/* Modal body */}
+              <div className="p-4 md:p-5 space-y-4 grid grid-cols-1 md:grid-cols-2 gap-10 h-full">
                 <div className="bg-[#0041ff] rounded-full hidden md:flex items-center justify-center flex-col h-[500px] py-3">
                   <Image
                     src={you}
                     alt=""
                     width={100}
                     height={100}
-                    className="rounded-full h-[100px] w-[100px] text-white "
+                    className="rounded-full h-[100px] w-[100px] text-white"
                   />
                   <p className="text-white text-[25px] py-5">Yess Ethiopia</p>
                   <p className="text-lg leading-relaxed text-white px-3 cri text-center">
@@ -38,24 +37,22 @@ const Don = () => {
                     expand our reach to help even more individuals in need.
                   </p>
                 </div>
-                <div className="pop text-gray-500 dark:text-gray-400 bg-white rounded-xl px-10 py-3 md:w-[300px]  lg:w-[400px]">
-               <Link href="https://www.paypal.com/donate/?hosted_button_id=QPXMVXM8C3SKS">
-               <div className="flex gap-4 font-bold text-xl items-center bg-blue-600 text-white w-full px-3 rounded py-2 pt-5 mb-5">
-          <p className="text-normal font-light text-center">For International Donation</p>
-         <div>
-         <Link href="https://www.paypal.com/donate/?hosted_button_id=QPXMVXM8C3SKS">
-            <Image
-              src={pay}
-              alt=""
-              width={50}
-              height={50}
-              className="rounded hover:opacity-80 transition-opacity duration-300"
-              title="Click Here"
-            />
-          </Link>
-          </div>
-          </div>
-               </Link>
+                <div className="pop text-gray-500 dark:text-gray-400 bg-white rounded-xl px-10 py-3 md:w-[300px] lg:w-[400px]">
+                  <Link href="https://www.paypal.com/donate/?hosted_button_id=QPXMVXM8C3SKS">
+                    <div className="flex gap-4 font-bold text-xl items-center bg-blue-600 text-white w-full px-3 rounded py-2 pt-5 mb-5">
+                      <p className="text-normal font-light text-center">For International Donation</p>
+                      <div>
+                        <Image
+                          src={pay}
+                          alt=""
+                          width={50}
+                          height={50}
+                          className="rounded hover:opacity-80 transition-opacity duration-300"
+                          title="Click Here"
+                        />
+                      </div>
+                    </div>
+                  </Link>
                   <div className="flex text-gray-800 justify-center items-center text-center gap-4">
                     <label>Phone</label>
                     <input
@@ -80,37 +77,36 @@ const Don = () => {
                       </select>
                     </div>
                   </div>
-                    <div className="flex gap-4 font-light justify-center items-center py-2">
-          <p className="text-center">For Ethiopia</p> <p className="font-extrabold text-xl mx-2">-</p><p className="text-center"> 1000 60 18 97 605 CBE</p>
-        </div>
+                  <div className="flex gap-4 font-light justify-center items-center py-2">
+                    <p className="text-center">For Ethiopia</p>
+                    <p className="font-extrabold text-xl mx-2">-</p>
+                    <p className="text-center">1000 60 18 97 605 CBE</p>
+                  </div>
                   <Link href="https://www.paypal.com/donate/?hosted_button_id=QPXMVXM8C3SKS">
-               <div className="flex gap-4 font-bold text-xl items-center bg-blue-600 text-white w-full px-3 rounded py-1  my-2">
-          <p className="text-normal font-light text-center">For International Donation</p>
-         <div>
-         <Link href="https://www.paypal.com/donate/?hosted_button_id=QPXMVXM8C3SKS">
-            <Image
-              src={pay}
-              alt=""
-              width={50}
-              height={50}
-              className="rounded hover:opacity-80 transition-opacity duration-300"
-              title="Click Here"
-            />
-          </Link>
-          </div>
-          </div>
-               </Link>
-                
+                    <div className="flex gap-4 font-bold text-xl items-center bg-blue-600 text-white w-full px-3 rounded py-1 my-2">
+                      <p className="text-normal font-light text-center">For International Donation</p>
+                      <div>
+                        <Image
+                          src={pay}
+                          alt=""
+                          width={50}
+                          height={50}
+                          className="rounded hover:opacity-80 transition-opacity duration-300"
+                          title="Click Here"
+                        />
+                      </div>
+                    </div>
+                  </Link>
                   <div className="text-center flex justify-center items-center py-5">
-                   <Link href="https://www.paypal.com/donate/?hosted_button_id=QPXMVXM8C3SKS">
-                   <button
-                      data-modal-hide="default-modal"
-                      type="button"
-                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[16px] pop px-10 py-2 "
-                    >
-                      Donate Now
-                    </button>
-                   </Link>
+                    <Link href="https://www.paypal.com/donate/?hosted_button_id=QPXMVXM8C3SKS">
+                        <button
+                          data-modal-hide="default-modal"
+                          type="button"
+                          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[16px] pop px-10 py-2"
+                        >
+                          Donate Now
+                        </button>
+                    </Link>
                   </div>
                 </div>
               </div>
