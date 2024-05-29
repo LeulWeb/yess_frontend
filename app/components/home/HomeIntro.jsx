b03c5ddbfdf54c823ad8edac8e275b7d97bc8724
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-05-29 08:44:06 +0300

    update the home page hero section

--- app/components/home/HomeIntro.jsx
+++ app/components/home/HomeIntro.jsx
@@ -10,7 +10,7 @@ const HomeIntro = () => {
       >
         <div className="absolute bg-black/30  bg-cover bg-no-repeat min-h-full lg:h-screen lg:overflow-y-hidden w-screen overflow-x-hidden">
           <div className="flex flex-col items-center justify-center mt-28 lg:mt-36 text-white py-10 lg:py-5  lg:mb-10">
-            <h1 className="text-[30px] lg:text-[40px] mt-28 text-center font-bold py-5">
+            <h1 className="text-[30px] lg:text-[40px] mt-28 lg:mt-10 text-center font-bold py-5">
               We empower the youth community <br /> through various means
             </h1>
             <p className="text-[18px] sm:text-[20px] lg:text-[25px] text-center pb-5">
